074d85c14e6a4451be1532db5750b89ef2d47ec3
sweet-Ines <ines@prohaszka.de>
2014-12-27 14:26:11 +0100

    Aufbau einer Datenbank (Ein- und Auslesen)

--- Meilenstein5/php/getFavorites.php
+++ Meilenstein5/php/getFavorites.php
@@ -1,7 +1,39 @@
     <?php
+    require_once ('konfiguration.php');
 $name = $_GET["favBtn"];
 
- if($name=="Musik Favoriten"){
+
+    $db_link = mysqli_connect (MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK);
+
+    try {
+        if ($name == "Musik Favoriten") {
+            $sql = "SELECT * FROM musik";
+
+            $db_erg = mysqli_query($db_link, $sql);
+            if (!$db_erg) {
+                die('Ungültige Abfrage!');
+            }
+        } elseif ($name == "Film Favoriten") {
+            $sql = "SELECT * FROM film";
+
+            $db_erg = mysqli_query($db_link, $sql);
+            if (!$db_erg) {
+                die('Ungültige Abfrage!');
+            }
+
+        }
+    }
+    catch (Exception $ex){
+        echo $ex=getMessage();
+    }
+    finally{
+        //Verbindung beenden
+        mysql_close($db_link);
+    }
+
+    /*
+     * Meilenstein 5
+     *  if($name=="Musik Favoriten"){
         $jsonData = file_get_contents("musik.json");
         $obj = (array) json_decode($jsonData,true);
         $alben = $obj["favoriten"];
@@ -23,4 +55,5 @@ $name = $_GET["favBtn"];
          }
      }
  }
+     */
 ?>
--- Meilenstein5/php/immdb.php
+++ Meilenstein5/php/immdb.php
@@ -1,7 +1,119 @@
 <?php
 
-
 $name=$_GET["Absenden"];
+
+if($name == "Film"){
+    require_once ('konfiguration.php');
+
+    //Film
+    $filmtitel=$_GET["filmtitel"];
+    $regie=$_GET["regie"];
+    $drehbuch=$_GET["drehbuch"];
+    $filmerscheinungsjahr=$_GET["filmerscheinungsjahr"];
+    $schauspieler=$_GET["schauspieler"];
+    $filmgenre=$_GET["filmgenre"];
+
+
+    if(($filmtitel == "") OR ($regie == "") OR ($drehbuch == "") OR ($filmerscheinungsjahr == "") OR ($schauspieler == "") OR ($filmgenre == "")){
+        echo "Fehler: Eintrag unvollständig.";
+        die;
+    }
+
+    //TODO
+    //Verbindung herstellen
+    $datenbank = mysqli_connect (MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK) or die ("Verbindung fehlgeschlagen: ".mysql_error());
+    mysqli_set_charset($datenbank, 'utf8');
+   // mysql_select_db("Datenbank-Name") or die ("Datenbank existiert nicht");
+
+    if ( $datenbank )
+    {
+        echo 'Verbindung erfolgreich: ';
+        //print_r( $db_link);
+
+        $abfrage = "SELECT COUNT(Fid) FROM film";
+        $ergebnis = mysql_query($abfrage);
+        $menge = mysql_fetch_row($ergebnis);
+
+
+        //TODO id+1 hinzufügen
+        //Daten in DB speichern
+        $sql_befehl = mysql_query("INSERT INTO film (Filmtitel, Regie, Drehbuch, Erscheinungsjahr, Schauspieler, Filmgenre) VALUES ('".$_GET["filmtitel"]."','".$_GET["regie"]."','".$_GET["drehbuch"]."','".$_GET["filmerscheinungsjahr"]."','".$_GET["drehbuch"]."','".$_GET["schauspieler"]."','".$_GET["filmgenre"]."')");
+
+
+        if($sql_befehl)
+        { echo "Ihr Eintrag wurde hinzugefügt."; }
+    }
+    else
+    {
+
+        die('Keine Verbindung möglich! ');
+    }
+
+
+//Verbindung beenden
+    mysql_close($datenbank);
+}
+
+if($name=="music"){
+
+    require_once ('konfiguration.php');
+
+    //Musik
+    $interpreter=$_GET["interpreter"];
+    $albumtitel=$_GET["albumtitel"];
+    $musikerscheinungsjahr=$_GET["musicerscheinungsjahr"];
+    $songs=$_GET["songs"];
+    $musikgenre=$_GET["musicgenre"];
+
+
+
+    if(($interpreter == "") OR ($albumtitel= "") OR ($musikerscheinungsjahr== "") OR ($songs == "") OR ($musikgenre == "")){
+        echo "Fehler: Eintrag unvollständig.";
+        die;
+    }
+
+    //TODO
+    //Verbindung herstellen
+    $datenbank = mysqli_connect (MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK) or die ("Verbindung fehlgeschlagen: ".mysql_error());
+    mysqli_set_charset($datenbank, 'utf8');
+   // mysql_select_db("Datenbank-Name") or die ("Datenbank existiert nicht");
+
+
+    if ( $datenbank )
+    {
+        echo 'Verbindung erfolgreich: ';
+        //print_r( $db_link);
+
+        $abfrage = "SELECT COUNT(Mid) FROM musik";
+        $ergebnis = mysql_query($abfrage);
+        $menge = mysql_fetch_row($ergebnis);
+
+        //TODO id+1 hinzufügen
+        //Daten in DB speichern
+        $sql_befehl = mysql_query("INSERT INTO musik (Interpreter, Albumutitel, Erscheinungsjahr, Songs, Musikgenre) VALUES ('".$_GET["interpreter"]."','".$_GET["albumtitel"]."','".$_GET["musicerscheinungsjahr"]."','".$_GET["songs"]."','".$_GET["musicgenre"]."')");
+
+
+        if($sql_befehl)
+        { echo "Ihr Eintrag wurde hinzugefügt."; }
+    }
+    else
+    {
+
+        die('Keine Verbindung möglich! ');
+    }
+//Verbindung beenden
+    mysql_close($datenbank);
+}
+
+
+
+
+
+
+
+//Meilenstein 5- Daten in eine txt Datei speichern
+/*
+ * $name=$_GET["Absenden"];
 //if($name=="film")
 
 //echo $_POST["FilmAbsenden"];
@@ -53,6 +165,7 @@ if($name=="music")
 
 }
 
+ */
 
 
 
